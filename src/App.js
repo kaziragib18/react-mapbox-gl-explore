b33e95745e1ea459770843bd8488fb20aa4e3831
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-21 01:23:19 +0600

    mapbox gl direction driving route explored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import MapDirection from './components/MapDirection';
 
 
 function App() {
   return (
     <div className="App">
-     
+      <MapDirection></MapDirection>
     </div>
   );
 }
--- src/components/MapDirection.js
+++ src/components/MapDirection.js
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
-
+//install
+//npm i mapbox-gl
+//npm i @mapbox/mapbox-gl-directions --force
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2F6aXJhZ2liMTgiLCJhIjoiY2t1enQ1Nnc3NjVuZzJvdDl5enQ1aWJiMyJ9.tSMpA0p7YcNIgpZ7F5aufA';
 
 const MapDirection = () => {
-      useEffect(()=>{
+      useEffect(() => {
             const map = new mapboxgl.Map({
                   container: 'map',
                   style: 'mapbox://styles/mapbox/streets-v11',
-                  center: [-79.4512, 43.6568],
+                  center: [91.82012551464157, 22.357733738899825],
                   zoom: 13
-                  });
-                   
-                  map.addControl(
+            });
+
+            map.addControl(
                   new MapboxDirections({
-                  accessToken: mapboxgl.accessToken
+                        accessToken: mapboxgl.accessToken
                   }),
                   'top-left'
-                  );
+            );
 
-      },[])
+      }, [])
       return (
             <div>
                   <div id="map"></div>
